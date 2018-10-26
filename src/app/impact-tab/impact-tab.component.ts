824ee3be3cb77d89e89f5f38f65ec7cb1827c8c0
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-10-26 21:50:17 +0200

    some content for the impact tab
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/app/impact-tab/impact-tab.component.ts
+++ src/app/impact-tab/impact-tab.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImpactTabComponent implements OnInit {
 
+  items = [
+    {
+      type : "no-powerty",
+      typeId : "01",
+      title : "No Powerty",
+      content : "Will save 10 children from starving in Yemen."
+    },
+    {
+      type : "good-health",
+      typeId : "03",
+      title : "Good Health",
+      content : "Will make people run at least 10 minutes a day."
+    },
+    {
+      type : "life-below-water",
+      typeId : "14",
+      title : "Life below water",
+      content : "Will feed the fish."
+    }
+  ];
+
   constructor() { }
+
+  imageSource(item) : string {
+    return "E_SDG_Icons-"+ item.typeId +".jpg";
+  }
 
   ngOnInit() {
   }
