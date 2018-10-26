83a5fdc82a65f873973340e3c9e04146627ba5d6
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-10-26 15:29:30 +0200

    go go
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ChartsModule } from 'ng2-charts';
 
-
 @NgModule({
   declarations: [
     AppComponent
